7bf4d8d0f0a17e4457b8564ddfbdd5b8333f9d20
manikeshhhh <manikesh182003@gmail.com>
2025-03-30 22:23:02 +0530

    initialized project

---
+++ src/main/java/com/singhmanikesh/securestash/SecurestashApplication.java
@@ -0,0 +1,13 @@
+package com.singhmanikesh.securestash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurestashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurestashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singhmanikesh/securestash/SecurestashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singhmanikesh.securestash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurestashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
